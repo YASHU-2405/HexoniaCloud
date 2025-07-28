f48cb178671951e6799630861592db1d37d803c9
YASHU-2405 <yt1250397@gmail.com>
2025-07-28 16:43:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def home():
-    return "🚀 Minecraft Hosting Panel Running!"
-if __name__ == "__main__":
+    return '🚀 Minecraft Hosting Panel Running!'
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
